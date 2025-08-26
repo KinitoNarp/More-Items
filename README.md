19c17e1f95ea97240251d9e616f363b720efb791
KinitoNarp <kinitonarp@proton.me>
2025-08-26 18:39:42 +0000

    Added Macarons

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Not Enough Things
 my cool fabric 1.20.1 mod that adds random shi
-this mod is opensource
+# this mod is opensource
---
+++ src/main/java/kinitonarp/dev/items/Macaron.java
@@ -0,0 +1,31 @@
+package kinitonarp.dev.items;
+
+import net.minecraft.item.FoodComponent;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+
+// public class Macaron {
+//    public static final FoodComponent MACARON = new FoodComponent.Builder().hunger(3).saturationModifier(0.2f).build();
+//}
+
+public class Macaron {
+    public static final Item CUSTOM_FOOD = registerItem("custom_food",
+            new Item(new FabricItemSettings().food(new FoodComponent.Builder()
+                    .hunger(6)
+                    .saturationModifier(0.6f)
+                    .build())));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, new Identifier("modid", name), item);
+    }
+
+    public static void registerModItems() {
+        System.out.println("Registering Mod Items for modid");
+    }
+}
