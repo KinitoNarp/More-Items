4cb04fc7038cd02101c2d03b6e482a76f5084975
KinitoNarp <kinitonarp@proton.me>
2025-08-26 10:42:22 +0000

    added actual mod files

---
+++ src/client/java/kinitonarp/dev/NotEnoughThingsClient.java
@@ -0,0 +1,10 @@
+package kinitonarp.dev;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NotEnoughThingsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
